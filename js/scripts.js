61e8354be177542ccdf4bac903c5314fd4aaa14b
Corrie Smith & Ryan Heupler <student@epicodus.com>
2015-09-14 12:08:38 -0700

    display contact info on click

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,20 @@
 $(function() {
   $("#add-address").click(function() {
-    $("#new-address").append('<div id="new-address">' +
+    $(".new-address").append('<div class="new-address">' +
     '<div class="form-group">' +
-    '<input type="text" placeholder="Address" class="form-control" id="address">' +
+    '<input type="text" placeholder="Address" class="form-control address"' +
     '</div>' +
 
     '<div class="form-group">' +
-    '<input type="text" placeholder="City" class="form-control" id="city">' +
+    '<input type="text" placeholder="City" class="form-control city"' +
     '</div>' +
 
     '<div class="form-group">' +
-    '<input type="text" placeholder="State" class="form-control" id="state">' +
+    '<input type="text" placeholder="State" class="form-control state"' +
     '</div>' +
 
     '<div class="form-group">' +
-    '<input type="text" placeholder="Zip" class="form-control" id="zip">' +
+    '<input type="text" placeholder="Zip" class="form-control zip"' +
     '</div>' +
     '</div>');
   });
@@ -30,36 +30,36 @@ $(function() {
       var inputtedAddress = $(this).find("input.address").val();
       var inputtedCity = $(this).find("input.city").val();
       var inputtedState = $(this).find("input.state").val();
-      var inputtedZip = parseInt($(this).find$("input.zip").val());
+      var inputtedZip = parseInt($(this).find("input.zip").val());
 
       newAddress = { address: inputtedAddress, city: inputtedCity, state: inputtedState, zip: inputtedZip };
       newContact.addresses.push(newAddress);
 
     });
 
-  $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li");
+  $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
     $("#result").show();
-  });
 
   $(".contact").last().click(function() {
     $("#show-contact").show();
 
-    $("#show-contact 2").text(newContact.firstName);
-    $(".firstName").text(newContact.firstName);
-    $(".lastName").text(newContact.lastName);
+    $("#show-contact h2").text(newContact.firstName);
+    $(".first-name").text(newContact.firstName);
+    $(".last-name").text(newContact.lastName);
 
     $("ul#addresses").text("");
     newContact.addresses.forEach(function(address) {
-      $("ul#addresses").append("<li>" + address.street + ", " + address.city + ", " +       address.state + ", " + address.zip +"</li>");
+      $("ul#addresses").append("<li>" + address.address + ", " + address.city + ", " + address.state + ", " + address.zip + "</li>");
     });
   });
 
   $("input#firstName").val("");
   $("input#lastName").val("");
-  $("input#address").val("");
-  $("input#city").val("");
-  $("input#state").val("");
-  $("input#zip").val("");
+  $("input.address").val("");
+  $("input.city").val("");
+  $("input.state").val("");
+  $("input.zip").val("");
 
 
+  });
 });
